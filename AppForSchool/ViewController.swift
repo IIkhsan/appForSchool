c4140da032a4f9ac528e74818508caa66ba1f730
Ilyas Ikhsanov <ilias.ihsanov@gmail.com>
2018-07-14 18:05:16 +0300

    Initial Commit

---
+++ AppForSchool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AppForSchool
+//
+//  Created by Ilyas on 14.07.2018.
+//  Copyright © 2018 itis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
